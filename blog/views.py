c2cad0c7f77dd4325e04ee716168ec114a2917a6
Grace Jiang <gracjiang@gmail.com>
2020-12-03 00:08:59 -0800

    splash page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
+
+# splash page
+def splash_page(request):
+    return render(request, 'blog/splash.html')
+
+# blog posts page
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# post details
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
